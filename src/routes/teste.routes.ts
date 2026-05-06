b15a71e3d433ac1eff666a0c16bda90bc5873d63
RyanCNP <ryan.cnp234@gmail.com>
2026-05-05 21:49:09 -0300

    fix: import getS3

--- src/routes/teste.routes.ts
+++ src/routes/teste.routes.ts
@@ -2,7 +2,7 @@ import { Request, Router,Response } from "express";
 import jwt from "jsonwebtoken";
 import dotenv from "dotenv";
 import { UsuarioModel } from "../models/usuario.model";
-import { getArquivoS3 } from "../utils/s3Client";
+import { getArquivoS3byID } from "../utils/s3Client";
 
 
 dotenv.config();
@@ -10,7 +10,7 @@ dotenv.config();
 const router = Router();
 
 router.get('/', async (req:Request, res:Response) => {
-  const response = await getArquivoS3('487a96b01acae527c51aaa9f1112c9f1');
+  const response = await getArquivoS3byID('487a96b01acae527c51aaa9f1112c9f1');
   res.json({ message: 'Teste de rota funcionando!', response });
 });
 export default router;
