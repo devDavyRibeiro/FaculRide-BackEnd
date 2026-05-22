f1a2aa14c250459a440f770d9bd93f737c1386aa
Herivelton <99099500+goncahri@users.noreply.github.com>
2026-05-22 00:00:36 -0300

    fix(s3): corrige variaveis de ambiente aws (#23)
    * feat(ci): adiciona deploy automatico do backend na EC2
    
    * fix(cors): libera swagger via ip da ec2
    
    * fix(cors): adiciona dns amigavel aws no cors
    
    * fix(auth): corrige leitura da senha no login
    
    * fix(auth): ajusta retorno do login no ambiente aws
    
    * fix(auth): valida id do usuario no login
    
    * fix(s3): corrige variaveis de ambiente aws

--- src/utils/s3Client.ts
+++ src/utils/s3Client.ts
@@ -10,9 +10,9 @@ import {
 export const s3 = new S3Client({
   region: process.env.AWS_REGION,
   credentials: {
-    accessKeyId: process.env.aws_access_key_id!,
-    secretAccessKey: process.env.aws_secret_access_key!,
-    sessionToken: process.env.aws_session_token!,
+    accessKeyId: process.env.AWS_ACCESS_KEY_ID!,
+    secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY!,
+    sessionToken: process.env.AWS_SESSION_TOKEN!,
   },
 });
 
