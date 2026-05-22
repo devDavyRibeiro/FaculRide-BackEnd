8cb77ca7c8caa7494184ed345835ef62b791e1e7
Herivelton Gonçalves <herivelton02@gmail.com>
2026-05-22 09:56:27 -0300

    fix(cloudwatch): corrige variaveis aws do logger

--- src/utils/cloudwatch.ts
+++ src/utils/cloudwatch.ts
@@ -2,8 +2,8 @@ import AWS from 'aws-sdk';
 
 AWS.config.update({
   region: process.env.AWS_REGION,
-  accessKeyId: process.env.AWS_ACCESS_KEY,
-  secretAccessKey: process.env.AWS_SECRET_KEY,
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
   sessionToken: process.env.AWS_SESSION_TOKEN
 });
 
